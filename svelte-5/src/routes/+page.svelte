bc3eb0acd2919a25444ae4aa6dac41695ba27cbe
David King <david@oodavid.com>
2025-03-31 10:59:31 +0100

    Tidy up before asking the q

--- svelte-5/src/routes/+page.svelte
+++ svelte-5/src/routes/+page.svelte
@@ -1,43 +1,33 @@
 <script lang="ts">
 	import { compile } from 'svelte/compiler';
-	import rawSource from './Source.svelte?raw';
-	import Source from './Source.svelte';
+	import Source from './Source.svelte'; // The compiled component
+	import rawSource from './Source.svelte?raw'; // The text to compile
 
 	let source = $state(rawSource);
 	let divElement: HTMLDivElement;
+	let isBusy = $state(false);
 
 	let myComponent: any;
 
 	const compileSource = async () => {
+		isBusy = true;
+		divElement.innerHTML = '';
 		const { js, css } = compile(source, {
 			runes: true,
 			compatibility: {
-				componentApi: 4
-			}
+				componentApi: 4 // Means we can do `new Component({ ... })`
+			},
+			customElement: true // Means the css is injected
 		});
 		console.log(js.code, css?.code);
 		const componentSrc = URL.createObjectURL(new Blob([js.code], { type: 'text/javascript' }));
-		// The error happens here
 		const { default: Component } = await import(/* @vite-ignore */ componentSrc);
-		// ...I've not managed to test anything beyond this point
 		URL.revokeObjectURL(componentSrc);
-		divElement.innerHTML = '';
-		// Attempt 1 - Use the `new` operator
 		myComponent = new Component({
 			target: divElement,
 			props: { arr: [4, 5, 6, 7, 8] }
 		});
-		console.log(myComponent);
-		// // Attempt 2 - Call the constructor directly
-		// Component(divElement, { arr: [1, 2, 3] });
-		// Attempt 3 - Use the mount function
-		// myComponent = mount(Component(), {
-		// 	target: divElement,
-		// 	props: { arr: [1, 2, 3] }
-		// });
-		// Attempt 4 - using custom elements
-		// console.log(Component);
-		// window.customElements.define('my-custom-element', Component());
+		isBusy = false;
 	};
 
 	const updateProps = () => {
@@ -49,13 +39,27 @@
 
 <svelte:head>
 	<!--
-		How did we build this list?
-		Via the package.json of the svelte package:
-	 	https://app.unpkg.com/svelte@5.25.3/files/package.json
+		How did we build this list? A few ways:
+		1. Read the svelte dependencies, and try to include them:
+	 	   https://app.unpkg.com/svelte@5.25.3/files/package.json
+		2. Watch the browser console, if it says something like "Failed to resolve module specifier..."
+		   then we check out the package on unpkg, scan the package.json
+		We could / should automate this, but for proof-of-contept, I'm doing it manually.
 	-->
 	<script type="importmap">
 		{
 			"imports": {
+				"svelte/internal/client": "https://cdn.jsdelivr.net/npm/svelte@5.25.3/src/internal/client/index.js",
+				"svelte/internal/disclose-version": "https://cdn.jsdelivr.net/npm/svelte@5.25.3/src/internal/disclose-version.js",
+				"svelte/internal/flags/legacy": "https://cdn.jsdelivr.net/npm/svelte@5.25.3/src/internal/flags/legacy.js",
+				"svelte/legacy": "https://cdn.jsdelivr.net/npm/svelte@5.25.3/src/legacy/legacy-client.js",
+				"esm-env": "https://cdn.jsdelivr.net/npm/esm-env@1.2.1/index.js",
+				"esm-env/browser": "https://cdn.jsdelivr.net/npm/esm-env@1.2.1/true.js",
+				"esm-env/development": "https://cdn.jsdelivr.net/npm/esm-env@1.2.1/false.js",
+				"esm-env/node": "https://cdn.jsdelivr.net/npm/esm-env@1.2.1/false.js",
+				"clsx": "https://cdn.jsdelivr.net/npm/clsx@2.1.1/dist/clsx.mjs"
+			},
+			"unused-unpkg-imports": {
 				"svelte/internal/client": "https://unpkg.com/svelte@5.25.3/src/internal/client/index.js",
 				"svelte/internal/disclose-version": "https://unpkg.com/svelte@5.25.3/src/internal/disclose-version.js",
 				"svelte/internal/flags/legacy": "https://unpkg.com/svelte@5.25.3/src/internal/flags/legacy.js",
@@ -70,19 +74,19 @@
 	</script>
 </svelte:head>
 
-<a href="./legacy">legacy mode (works)</a>
-
 <h1>Svelte 5 - Compiler</h1>
 <p>See: https://stackoverflow.com/a/73339006</p>
 <textarea style="field-sizing:content" bind:value={source}></textarea>
-<button onclick={compileSource}>Compile</button>
-<button onclick={updateProps}>Change props</button>
+<button onclick={compileSource} disabled={isBusy}>Compile</button>
+<button onclick={updateProps} disabled={isBusy}>Change props</button>
 
-<p>Compiled</p>
+{#if isBusy}
+	<p>Compiling... (check your network tab)</p>
+	<img src="https://imgs.xkcd.com/comics/compiling.png" alt="Compiling..." height="180" />
+{/if}
 <div bind:this={divElement}></div>
-<my-custom-element></my-custom-element>
 
 <hr />
 
-<p>Expected</p>
+<p>Expected (ie: pre-compiled)</p>
 <Source />
